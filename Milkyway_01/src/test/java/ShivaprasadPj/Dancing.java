c7f7e0b77b1c24c1ea51159dd72e8d8509f6dc25
user <user@DESKTOP-4UL57U2>
2025-07-01 13:17:15 +0530

    Added Dance class

---
+++ Milkyway_01/src/test/java/ShivaprasadPj/Dancing.java
@@ -0,0 +1,13 @@
+package ShivaprasadPj;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Dancing {
+	@Test
+	public void dance() {
+		WebDriver driver=new ChromeDriver();
+	}
+
+}
